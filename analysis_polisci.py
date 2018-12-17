76887578234d561095ac139d4ad526b05e335d00
Jeong Jae Kim <jjjkj0480@snu.ac.kr>
2018-12-17 14:43:07 +0900

    Final Turnin

--- analysis_polisci.py
+++ analysis_polisci.py
@@ -122,8 +122,20 @@ def show_contents(articles):
         print("Fact Check: " + verasity)
         print("-------------------------")
 
+def write_contents(articles, file):
+    for article in articles:
+        date = article[1]
+        title = article[2]
+        claim = article[3]
+        verasity = article[4]
+        file.write("Title: " + title + "\n")
+        file.write("Date: " + date + "\n")
+        file.write("Claim: " + claim + "\n")
+        file.write("Fact Check: " + verasity + "\n")
+        file.write("-------------------------" + "\n")
+
 def show_by_verasity(data):
-    print("-- For all articles from 2016 ~ 2018 --")
+    print("-- For all articles in 2016")
     for i in verasities:
         temp = 0
         for article in data:
@@ -161,7 +173,8 @@ if __name__ == '__main__':
  
     show_by_category(data) # 그림1
     show_by_verasity(data) # 그림2
-    data_politics = get_articles_of_category("politics", data)
+    data_politics = get_articles_of_category("politic", data)
+    print(len(data_politics))
     show_by_verasity(data_politics) # 그림3
 
     # 연도별 데이터
@@ -169,8 +182,55 @@ if __name__ == '__main__':
     data2017 = get_articles_of_2017(data)
     data2018 = get_articles_of_2018(data)
 
+    data_science = get_articles_of_category("science", data)
+    data_medicine = get_articles_of_category("medic", data)
+    data_sci = data_science + data_medicine # 중복 제거
+    show_by_verasity(data_sci)
+
+    mixture_poli = get_articles_of_verasity("mixture", data_politics)
+    #file1 = open("text1.txt", "w")
+    #write_contents(mixture_poli, file1) # text1 creation
+
+    data_politicians = get_articles_of_category("politicians", data)
+    print(len(data_politicians))
+    show_by_verasity(data_politicians)
+    #file2 = open("text2.txt", "w")
+    #write_contents(data_politicians, file2) # text2 creation
+
+    
+    #
+    f_attribute = get_articles_of_verasity("misattributed", data)
+    t_attribute = get_articles_of_verasity("correct-attribution", data)
+    f_captioned = get_articles_of_verasity("miscaptioned", data_politics)
+    print("Attribution articles:")
+    print("  False: " + str(len(f_attribute)))
+    print("  True: " + str(len(t_attribute))) # <그림6>
+    print("Photo News: ")
+    print("  False: " + str(len(f_captioned)))
+    
     trump = get_articles_of_keyword("Trump", data2016)
     trump_politics = get_articles_of_category("politics", trump)
+    clinton = get_articles_of_keyword("Clinton", data2016)
+    clinton_politics = get_articles_of_category("politics", clinton)
+    republic = get_articles_of_keyword("republic", data2016)
+    republic_politics = get_articles_of_category("politics", republic)
+    democrat = get_articles_of_keyword("democrat", data2016)
+    democrat_politics = get_articles_of_category("politics", democrat)
     show_contents(trump_politics)
+    print("Number of Articles in 2016: ")
+    print("Total: " + str(len(data2016)))
+    print("Trump: " + str(len(trump)))
+    print("Clinton: " + str(len(clinton)))
+    print("Republican Party: " + str(len(republic)))
+    print("Democratic Party: " + str(len(democrat))) # <그림7>
+
+    print("Donald Trump")
+    show_by_verasity(trump)
+    print("Hillary Clinton")
+    show_by_verasity(clinton)
     
+    file_t = open("text_t.txt", "w")
+    write_contents(trump, file_t) # trump text creation
+    file_c = open("text_c.txt", "w")
+    write_contents(clinton, file_c) # clinton text creation
     
